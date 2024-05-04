dcb9196d5ed322009301acba98c95058cfdc4f67
maxim <maximlitvinov.2611@gmail.com>
2024-05-04 23:53:50 +0300

    fix: rebuild navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -21,28 +21,35 @@ const Navbar = () => {
       {isMobile ? (
         <MobileNavbar />
       ) : (
-        <nav className="lg:w-full md:w-full md:mx-auto  flex lg:justify-center md:text-sm lg:gap-12 md:gap-24 xl:justify-between py-5 px-12">
-          <div className="max-w-44 flex gap-2 items-center cursor-pointer">
-            <Image src="/logo.svg" alt="logo" width={32} height={32} />
-            <span
-              className={`${spaceMono.className} text-cyan-50 hover:text-violet-400 duration-300`}
-            >
-              NFT Marketplace
-            </span>
+        <nav className="mx-auto max-w-[80rem] py-4">
+          <div className="flex justify-between">
+            <div className="w-fit flex gap-2 items-center cursor-pointer">
+              <Image src="/logo.svg" alt="logo" width={32} height={32} />
+              <span
+                className={`${spaceMono.className} text-cyan-50 hover:text-violet-400 duration-300`}
+              >
+                NFT Marketplace
+              </span>
+            </div>
+
+            <div className="">
+              <ul
+                className={`${workSans.className}  flex gap-6 items-center justify-between  text-cyan-50`}
+              >
+                {navBarItems.map((el) => (
+                  <li className="hover:text-violet-400 duration-300" key={el}>
+                    <a href="#">{el}</a>
+                  </li>
+                ))}
+                <div className="">
+                  <PrimaryButton>
+                    <FaRegUser size={20} />
+                    Sign Up
+                  </PrimaryButton>
+                </div>
+              </ul>
+            </div>
           </div>
-          <ul
-            className={`${workSans.className} xl:max-w-[30rem] lg:w-1/2 md:w-3/4 items-center flex justify-between xl:gap-3 lg:gap-1 text-cyan-50`}
-          >
-            {navBarItems.map((el) => (
-              <li className="hover:text-violet-400 duration-300" key={el}>
-                <a href="#">{el}</a>
-              </li>
-            ))}
-            <PrimaryButton>
-              <FaRegUser size={20} />
-              Sign Up
-            </PrimaryButton>
-          </ul>
         </nav>
       )}
     </>
